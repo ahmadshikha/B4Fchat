ae33abd12068544126198034416791451fc2a580
Malkalteer <alteermalk@gmail.com>
2024-06-03 10:51:23 +0300

    fixed a bug

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,31 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './output.css';
 import HomePage from './pages/HomePage';
-import InfoPage from './pages/InfoPage';
 
 
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="">
-{/*      
-     <div className='group/yazan'>
-      <h2 className=' bg-slate-500'>hello world</h2>
-
-      <h3 className='group-hover/yazan:text-red-600'>wellcome to ghaith</h3>
-     </div>
-     <h1>wellcome</h1> */}
-     
-     {/* <HomePage/> */}
-     <InfoPage/>
-
-=======
     <div className="App h-screen">
       <Routes>
           <Route path='/' element={<HomePage/>}/>
       </Routes>
->>>>>>> bc877cc2ee2ee89fa7e9cc242eddf4d372505882
     </div>
   );
 }
