66c5892720a52b9bf35460cbd6aad38e227af519
Medo <moaiadefahed2000@gmail.com>
2024-06-04 21:51:25 +0300

    adss

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,20 +9,15 @@ import Slider from './Components/Slider';
 
 function App() {
   return (
-
-    <div className="">
-
     <div className="App h-screen">
       <Routes>
         <Route path='/info' element={<InfoPage/>}/>
           <Route path='/' element={<HomePage/>}/>
           <Route path='/WelcomePage' element={<WelcomePage/>}/>
           <Route path='/VerificationPage' element={<VerificationPage/>}/>
-          <Route path='/InfoPage' element={<InfoPage/>}/>
-
+          <Route path='/slider' element={<Slider/>}/>
       </Routes>
 
-    </div>
     </div>
   );
 }
