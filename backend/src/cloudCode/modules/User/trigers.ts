c8a651540ce0b9da5b60333692c3f0c9444bed0b
Yazan <yazansamer123qwe@gmail.com>
2024-06-09 17:40:35 +0300

    minor fix

--- backend/src/cloudCode/modules/User/trigers.ts
+++ backend/src/cloudCode/modules/User/trigers.ts
@@ -1,1 +1,8 @@
- 
+Parse.Cloud.beforeSave(
+    Parse.User,
+    async req =>{
+        const object = req.object;
+        const acl = new Parse.ACL();
+        acl.setPublicReadAccess(true)
+        object.setACL(acl)
+    })
